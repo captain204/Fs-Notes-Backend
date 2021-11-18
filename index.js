9317d748d9b49a86101f2ba2d170a8410b9be964
Nurudeen <nurudeenakindele8@gmail.com>
2021-11-18 18:01:59 +0100

    Cors Installation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { response, request } = require('express')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 let notes = [
     {
@@ -40,12 +42,28 @@ app.get('/api/notes',(request, response)=>{
     response.json(notes)
 })
 
-app.post('/api/notes',(request,response)=>{
+const generatedId = ()=>{
     const maxId = notes.length > 0 
                   ? Math.max(...notes.map(n => n.id))
                   :0
-    const note = request.body
-    note.id = maxId + 1
+    return maxId + 1
+}
+
+app.post('/api/notes',(request,response)=>{
+    const body = request.body
+    
+    if(!body.content){
+      return response.status(400).json({
+        error:'content missing'
+      })
+    }
+
+    const note = {
+        content:body.content,
+        important:body.important || false,
+        date:new Date(),
+        id:generatedId()
+    }
 
     notes = notes.concat(note)
     console.log(note)
