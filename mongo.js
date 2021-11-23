125e5f35444fb1929b5b4f8172e10f03392afe3c
Nurudeen <nurudeenakindele8@gmail.com>
2021-11-23 15:15:50 +0100

    Mongo Js Added

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3){
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://captain:${password}@cluster0.n9dsd.mongodb.net/notes-app?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content:String,
+    date:Date,
+    important:Boolean
+})
+
+const Note = mongoose.model('Note',noteSchema)
+
+/*const note = new Note({
+    content: 'Leaders must make hard decisions',
+    date: new Date(),
+    important: true,
+})
+note.save().then(result=>{
+    console.log('Saved!')
+    mongoose.connection.close()
+})
+
+*/
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
+
+
