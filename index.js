74807c7da36ab6e104971472369eaf61aad01712
Nurudeen <nurudeenakindele8@gmail.com>
2021-11-30 12:15:30 +0100

    Mongose Validation

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ app.use(cors())
 app.use(express.static('build'))
 
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }else if(error.name === 'ValidationError'){
+        return response.status(400).json({error:error.message})
+    }
+    next(error)
+}
+
+
 const Note = require('./models/note')
 
 app.get('/',(request,response) => {
@@ -30,7 +41,7 @@ app.get('/api/notes',(request, response)=>{
     return maxId + 1
 }*/
 
-app.post('/api/notes',(request,response)=>{
+app.post('/api/notes',(request,response,next)=>{
     const body = request.body
     
     if(body.content === undefined){
@@ -45,22 +56,48 @@ app.post('/api/notes',(request,response)=>{
         date:new Date(),
     })
 
-    note.save().then(savedNote=>{
-        response.json(savedNote)
-    })
+    note.save().
+    then(savedNote=>savedNote.toJSON())
+    .then(savedFormattedNote=>{
+        response.json(savedFormattedNote)
+    }).catch(error => next(error))
 })
 
-app.get('/api/notes/:id',(request,response)=>{
+app.get('/api/notes/:id',(request,response,next)=>{
     Note.findById(request.params.id).then(note=>{
-        response.json(note)
+        if(note){
+            response.json(note)
+        }else{
+            response.status(404).end()
+        }
     })
+    .catch(error=>next(error))
 })
 
 app.delete('/api/notes/:id',(request,response)=>{
-    const id = Number(request.params.id)
-    notes = notes.filter(note=>note.id !==id)
-    response.status(204).end()
+    Note.findByIdAndRemove(request.params.id)
+    .then(result=>{
+        response.status(204).end()
+    })
+    .catch(error=>next(error))
 })
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+    
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+        response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+    
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT 
 app.listen(PORT, ()=>{
